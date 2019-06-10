06dce3ff1cf17b5cef5f6abd12f0fd77f434940f
Farnoush Yousefi Kejani <farnoushyousefik@gmail.com>
2019-06-09 23:27:07 -0400

    linkList

--- main.cpp
+++ main.cpp
@@ -1,36 +1,29 @@
-class Solution {
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
-public:
-
-    vector<vector<int> > threeSum(vector<int> &num) {
-
-        set<vector<int>> rs;
-        sort(num.begin(),num.end());
-        int i, j, k;
-        int n=num.size();
-        for(int i=0;i<n-2;i++){
-            j=i+1;
-            k=n-1;
-            while(j<k){
-                int sum=num[i]+num[j]+num[k];
-                if(sum==0){
-                    vector<int>tmp(3);
-                    tmp[0]=num[i];
-                    tmp[1]=num[j];
-                    tmp[2]=num[k];
-                    rs.insert(tmp);
-                    j++;
-                    k--;
-                }else if(sum>0){
-                    k--;
-                } else if(sum<0){
-                    j++;
-                }
-            }
-        }
-      vector<vector<int> result(rs.begin(),rs.end());
-      return result;
-    }
-
-};
-
+using namespace std;
+ Struct Node{
+     int data;
+     Node* link;
+ }
+ int main(){
+     Node* A;
+     A=NULL;
+     Node* temp=new Node(0);
+     temp->data=2;
+     temp->link=NULL;
+     A=temp;
+     
+     
+     temp=new Node();
+     temp->data=4;
+     temp->link=NULL;
+     
+     Node* temp1=A;
+     while(temp1->link!=NUll){
+         temp1=temp1->link
+     }
+     temp1->link=temp;
+ }
